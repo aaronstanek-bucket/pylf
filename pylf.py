2dda50a04fac9550d9fea4dc93bf29b3e2cefcf3
Aaron Stanek <Aaron Stanek>
2017-02-24 11:40:08 -0800

    changed file format to now have 18,19,20 for different container types, updated interpreter accordingly, not tested yet

--- pylf.py
+++ pylf.py
@@ -14,8 +14,10 @@
 #3 is bool
 #4 is string
 
-#17 is start of list
+#17 is start of list, dict, or set
 #18 is end of list
+#19 is end of dict
+#20 is end of set
 
 #lists are encoded as lists with the first element as "L"
 #dicts are encoded as lists with the first element as "D", followed by
@@ -48,23 +50,20 @@ def encode(data, ou):
         ou.append(4)
     elif t==list:
         ou.append(17)
-        encode("L",ou)
         for x in data:
             encode(x,ou)
         ou.append(18)
     elif t==dict:
         ou.append(17)
-        encode("D",ou)
         for x in data:
             encode(x,ou)
             encode(data[x],ou)
-        ou.append(18)
+        ou.append(19)
     elif t==set:
         ou.append(17)
-        encode("S",ou)
         for x in data:
             encode(x,ou)
-        ou.append(18)
+        ou.append(20)
     else:
         raise ValueError
 
@@ -85,22 +84,14 @@ def toFile(filename, data):
 def decode_dict(data):
     #data is a list
     ou={}
-    i=1
+    i=0
     dataLen=len(data)
-    mode=0
-    key=""
-    value=""
     while i<dataLen:
-        if mode==0:
-            #this is a key
-            key=data[i]
-            mode=1
-        else:
-            #this is a value
-            value=data[i]
-            ou[key]=value
-            mode=0
+        key=data[i]
         i=i+1
+        value=data[i]
+        i=i+1
+        ou[key]=value
     return ou
 
 def decoder(data):
@@ -108,28 +99,9 @@ def decoder(data):
     k=[] #holds all lists
     s="" #holds strings while being processed
     for x in data:
-        if x==17:
-            k.append([])
-        elif x==18:
-            if len(k)==0:
-                return []
-            elif len(k)==1:
-                return k[0][1:] #we want to chop off the first element
-            else:
-                j=k[-1][0]
-                #j should be a string, L, D, S
-                if j=="L":
-                    k[-2].append(k[-1][1:]) #we want to chop off the first element
-                elif j=="D":
-                    k[-1]=decode_dict(k[-1])
-                    k[-2].append(k[-1])
-                elif j=="S":
-                    k[-1]=set(k[-1][1:]) #again, chop off first element
-                    k[-2].append(k[-1])
-                else:
-                    raise ValueError
-                k.pop()
-                del(j)
+        if x>20:
+            #this was added increase speed, remove this if statement if editing this file
+            s=s+chr(x)
         elif x==1:
             k[-1].append(int(s))
             s=""
@@ -147,8 +119,28 @@ def decoder(data):
         elif x==4:
             k[-1].append(s)
             s=""
+        elif x==17:
+            k.append([])
+        elif x==18:
+            #end of list
+            if len(k)==0:
+                return []
+            if len(k)==1:
+                return k[0]
+            #hop list down a level
+            k[-2].append(k[-1])
+            k.pop()
+        elif x==19:
+            #end of dict
+            k[-2].append(decode_dict(k[-1]))
+            k.pop()
+        elif x==20:
+            #end of set
+            k[-2].append(set(k[-1]))
+            k.pop()
         else:
             #just add to the stack
+            #this bit only gets run if we have a value less than 20 and not a command character
             s=s+chr(x)
     #it should have returned by now, but just to be sure
     return []
